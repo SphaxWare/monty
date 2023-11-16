2a07ae1c613d56c38766e773cff8880f4efa15a8
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-11-16 04:13:43 +0100

    Handles freeing of stack

--- free_stack.c
+++ free_stack.c
@@ -1,1 +1,22 @@
-i
+#include "monty.h"
+
+/**
+ * free_stack - frees the stack
+ * @head: pointer to first node in stack
+ * Description - frees stack in form of a doubly linked list
+ * Return: void
+*/
+
+void free_stack(stack_t *head)
+{
+	stack_t *iter;
+	stack_t *store_next;
+
+	iter = head;
+	while (iter != NULL)
+	{
+		store_next = iter->next;
+		free(iter);
+		iter = store_next;
+	}
+}
