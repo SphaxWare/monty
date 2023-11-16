b7c183337d3b840f5faf3d1d1820c5c8b11aa8bb
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-11-16 04:32:34 +0100

    Edits function description of file pint.c

--- pint.c
+++ pint.c
@@ -1,8 +1,11 @@
 #include "monty.h"
+
 /**
  * pint - this function prints the value at the top of the stack.
- * @stack:ouble pointer to the stack.
+ * @stack: pointer to the stack.
  * @line_number: line number.
+ * Description - prints the integer value at the top of the stack.
+ * Return: void
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
