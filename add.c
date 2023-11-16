81fb511ee11d5cc24cf5bb6bb50d91d730ae5cee
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-11-16 11:10:17 +0100

    Edit file add.c to change error message

--- add.c
+++ add.c
@@ -16,7 +16,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (stack_len(*stack) < 2)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n",
+		fprintf(stderr, "L%d: can't add, stack too short\n",
 		line_number);
 		exit(EXIT_FAILURE);
 	}
